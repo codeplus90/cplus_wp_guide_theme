7b8e9fdccb433b257b595c645478c59abf74e6b8
codeplus90 <simonefontana1990@yahoo.it>
2017-01-27 22:45:35 +0100

    Lezione 3
    Creazione dell'header e richiamo in index

---
+++ codeplus-theme/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+      <title> <?php bloginfo('name'); ?> </title>
+      <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>"/>
+      <!--Stylesheet Theme, Bootstrap and JQuery-->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+      <?php wp_head(); ?>
+    </head>
+    <body>
