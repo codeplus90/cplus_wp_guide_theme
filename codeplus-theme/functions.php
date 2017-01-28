e7a97baee0fd5322e7831f7d5862232e1e243e36
codeplus90 <simonefontana1990@yahoo.it>
2017-01-28 23:36:52 +0100

    Lezione 6
    Aggiunto register menu al file function

--- codeplus-theme/functions.php
+++ codeplus-theme/functions.php
@@ -2,4 +2,9 @@
 
 add_theme_support( 'custom-header' );
 
+function register_my_menu() {
+                        register_nav_menu('primary',__( 'Primary menu' ));
+}
+add_action( 'init', 'register_my_menu' );
+
 ?>
