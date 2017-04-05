41bfd808fad093fe45652c7237b5e51446880b6b
codeplus90 <simonefontana1990@yahoo.it>
2017-04-05 16:10:34 +0200

    Lezione 11
    Aggiunto Wp_query alla sezione news

--- codeplus-theme/news-section.php
+++ codeplus-theme/news-section.php
@@ -1,18 +1,19 @@
 <section id="news" class="section-home">
     <div class="container">
         <h2 class="text-center">Ultime News
-         <?php if ( have_posts() ) :
-                   while ( have_posts() ) : the_post(); ?>
-                  <div class="col-md-4">
+         <?php $query = new WP_Query(array( 'category_name'  => 'news',
+                                            'posts_per_page' =>  3, ));
+               if ($query->have_posts()) :
+                  while ($query->have_posts()) : $query->the_post(); ?>
+                    <div class="col-md-4">
                       <div class="container-news">
                         <a href="">
                             <h2 class="text-center">
-
-                           <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+                           <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
                            <img class="image-news" src="" alt=""/>
                         </a>
                       </div>
-                   </div>
+                     </div>
                    <?php endwhile; ?>
         <?php endif; ?>
     </div>
